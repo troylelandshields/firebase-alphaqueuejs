36c7b3ad71ca1f5d84fddb857c177ad0b88f67bb
troylelandshields <troy.leland.shields@gmail.com>
2016-08-11 20:14:08 -0600

    Trying to make this thing work in the browser too

--- firebase-alphaqueue.js
+++ firebase-alphaqueue.js
@@ -1,46 +1,75 @@
-function AlphaQueue(firebaseRef, tasks) {
-    var q = this
+"use strict";
 
-    q.tasksRef = firebaseRef.child("queue").child("tasks")
+(function () {
+    var root = this
 
-    tasks.forEach(function (t) {
-        q[t] = sendToQueueTask(q.tasksRef, t)
-    })
-}
+    function AlphaQueue(firebaseTasksRef, tasks) {
+        var q = this
 
-function sendToQueueTask(tasksRef, taskName) {
-    return function (data) {
-        console.log("pushing to queue", data)
-        resultPromise = new Promise(function (resolve, reject) {
-            data._state = startStateName(taskName)
-            taskRef = tasksRef.push(data)
+        //Store reference to tasks in firebase queue
+        q.tasksRef = firebaseTasksRef;
 
-            console.log("awaiting results")
-            tasksRef.child(taskRef.key).child("_state").on('value', function (dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    if (dataSnapshot.val() == endStateName(taskName)) {
-                        resolve(dataSnapshot.val())
-                    } else if (dataSnapshot.val() == errorStateName(taskName)) {
-                        reject(dataSnapshot.val())
-                    }
-                }
-            });
-        });
-
-        return resultPromise
+        //Create a function for pushing and receiving to each task in the queue
+        tasks.forEach(function (t) {
+            q[t] = sendToQueueTask(q.tasksRef, t)
+        })
     }
-}
 
-function startStateName(taskName) {
-    return taskName + "_start"
-}
+    //TODO clean this up
+    function sendToQueueTask(tasksRef, taskName) {
+        return function (data) {
+            //Create promise that will be resolved or rejected when the task enters a final state
+            var resultPromise = new Promise(function (resolve, reject) {
+                //Start the task in the starting state and push it onto the queue
+                data._state = startStateName(taskName)
+                var taskRef = tasksRef.push(data)
 
-function endStateName(taskName) {
-    return taskName + "_finished"
-}
+                //Watch for the state to change--when it is either error or finished then resolve or reject the promise with the appropriate data
+                tasksRef.child(taskRef.key).child("_state").on('value', function (dataSnapshot) {
 
-function errorStateName(taskName) {
-    return taskName + "_error"
-}
+                    if (dataSnapshot.exists()) {
+                        if (dataSnapshot.val() == endStateName(taskName)) {
+                            tasksRef.child(taskRef.key).once('value', function (taskSnapshot) {
+                                var taskData = taskSnapshot.val()
+                                resolve(taskData);
+                            });
+                        } else if (dataSnapshot.val() == errorStateName(taskName)) {
+                            tasksRef.child(taskRef.key).once('value', function (taskSnapshot) {
+                                var taskData = taskSnapshot.val()
+                                reject(taskData._error_details);
+                            });
+                        }
 
-module.exports = AlphaQueue
+                    }
+                });
+            });
+
+            return resultPromise
+        }
+    }
+
+    function startStateName(taskName) {
+        return taskName + "_start"
+    }
+
+    function endStateName(taskName) {
+        return taskName + "_finished"
+    }
+
+    function errorStateName(taskName) {
+        return taskName + "_error"
+    }
+
+    //Magic to export this for node or make it available on the window for browser
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = AlphaQueue
+        }
+        exports.AlphaQueue = AlphaQueue
+    }
+    else {
+        root.AlphaQueue = AlphaQueue
+    }
+
+
+}).call(this);
